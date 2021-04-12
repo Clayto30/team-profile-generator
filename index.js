2dbfe02a77d15c3cb742ffcf0d0dc809d46c1acc
Clayton Goff <cltngoff@gmail.com>
2021-04-12 00:14:21 -0700

    all questions are functioning

--- index.js
+++ index.js
@@ -2,30 +2,50 @@ const inquirer = require('inquirer');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
-// create an empty array of team members and push this manager (with their team members in it too)
 const employeesArray = [];
-let recursionIncrement = 0;
-const createEmployees = () => {
-    // when this is done and I'm sure I don't have an infinite loop, remove recursion incredment and return
-    recursionIncrement++
-    if (recursionIncrement > 20) {
-        return
-    }
+// creates an employee type that is an intern
+const createIntern = () => {
     inquirer
         .prompt([
-            
-            // { type: 'list',
-            //     name:
-            //     message "what "
-            //     choices : "engineer", intern, finish building my team}
-            // do a prompt for jus the above question.
-            // .then(res) {
-                // if (next) = "finish building my team" call a function
-                // else if (next = "engineer")  ask em more questions
-            //}
-            // 
-                {
-            
+            {
+
+                type: 'input',
+                name: 'name',
+                message: "Please enter the employee's name.",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true
+                    } return `Please enter a character as a string.`
+                }
+            },
+            {
+                type: 'input',
+                // is a number above zero 
+                name: 'id',
+                message: "What is this employee's ID number?"
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Please enter the employee's email."
+                // validate is a valid email address by checking if string
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "What is the intern's school?",
+            }])
+        .then(answers => {
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+            promptNewEmployee();
+        })
+}
+// creates an employee object that is an engineer
+const createEngineer = () => {
+    inquirer
+        .prompt([
+            {
+
                 type: 'input',
                 name: 'name',
                 message: "Please enter the employee's name.",
@@ -51,20 +71,38 @@ const createEmployees = () => {
                 type: 'input',
                 name: 'github',
                 message: "What is the engineer's github profile?",
-                when: answers => {
-                    answers.employeeType === "Engineer"
-                }
-            },
-            {
-                type: 'input',
-                name: 'school',
-                message: "What is the intern's school?",
-                when: answers => {
-                    answers.employeeType === "Intern"
-                }
             }])
+        .then(answers => {
+            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            promptNewEmployee();
+        });
+}
+
+// questions user in CLI if they want to make another employee object
+const promptNewEmployee = () => {
+    inquirer
+        .prompt(
+
+            {
+                type: 'list',
+                name: 'employeeType',
+                message: "Would you like to add an employee?",
+                choices: ["engineer", "intern", "finish building my team"]
+            }
+        )
+        .then(answers => {
+            if (answers.employeeType == "engineer") {
+                createEngineer();
+            } else if (answers.employeeType == "intern") {
+                createIntern();
+            } else {
+                console.log("no more questions");
+                return;
+            };
+        })
 };
 
+// first employee created is the manager, with questions
 const createManager = () => {
 
     inquirer
@@ -102,11 +140,11 @@ const createManager = () => {
             const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
             // push manager to employeeArray here?
             // do this same thing 
-            createEmployees();
+            promptNewEmployee();
         });
 
 };
-
+// initial call 
 createManager();
 
 const buildTeam = () => {
